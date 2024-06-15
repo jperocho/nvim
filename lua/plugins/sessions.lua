9ab2f70921c17f213ad102c5c19fcf4943e7de0d
John Mark Perocho <per.eight@gmail.com>
2024-06-15 23:59:58 +0800

    added git sessions plugin

---
+++ lua/plugins/sessions.lua
@@ -0,0 +1,25 @@
+return {
+  "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup({
+      auto_session_suppress_dirs = {
+        "~/",
+        "~/Projects",
+        "~/Downloads",
+        "~/Desktop",
+        "~/Documents",
+        "~/Pictures",
+        "~/Videos",
+        "~/Music",
+        "/tmp",
+      },
+      session_lens = {
+        buftypes_to_ignore = {},
+        load_on_startup = true,
+        theme_conf = { border = true },
+        previewer = true,
+      },
+      vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, { noremap = true })
+    })
+  end,
+}
