7a2937ce9344977423d67be8b8fb7912e9b6b502
John Mark Perocho <per.eight@gmail.com>
2024-06-15 09:33:57 +0800

    added net_tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,22 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    local neo_tree = require("neo-tree")
+    neo_tree.setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          never_show = {
+            ".git",
+            ".DS_Store",
+            "thumbs.db",
+            ".vscode",
+          },
+        },
+      },
+    })
     vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", { desc = "Toggles Neotree" })
   end,
 }
