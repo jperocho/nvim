5677fb132ddc040e43e3932834d67d9efad30d9e
John Mark Perocho <per.eight@gmail.com>
2024-06-16 09:50:02 +0800

    fixed lint issue in lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,53 +1,53 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      local mason = require("mason")
-      mason.setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({
-        init_options = {
-          preferences = {
-            disableSuggestions = true,
-          },
-        },
-      })
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			local mason = require("mason")
+			mason.setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({
+				init_options = {
+					preferences = {
+						disableSuggestions = true,
+					},
+				},
+			})
 
-      local map = vim.keymap.set
-      map("n", "K", vim.lsp.buf.hover, {})
-      map("n", "gd", vim.lsp.buf.definition, {})
-      map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-      local null_ls = require("null-ls")
+			local map = vim.keymap.set
+			map("n", "K", vim.lsp.buf.hover, {})
+			map("n", "gd", vim.lsp.buf.definition, {})
+			map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          require("none-ls.diagnostics.eslint_d"),
-        },
-      })
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					require("none-ls.diagnostics.eslint_d"),
+				},
+			})
 
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
-  },
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
 }
