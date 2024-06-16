bb89b5e86ac7cae92459e8f9113fd8fafe2c513d
John Mark Perocho <per.eight@gmail.com>
2024-06-16 12:06:09 +0800

    modified mappings for find files in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
       local builtin = require("telescope.builtin")
       local map = vim.keymap.set
 
-      map("n", "<C-p>", builtin.find_files, {})
+      map("n", "<leader>ff", builtin.find_files, {})
       map("n", "<leader>fg", builtin.live_grep, {})
     end,
   },
